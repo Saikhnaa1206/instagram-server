df546bcedf069ef478f3197aec34c228a48f7f51
admin <admin@admins-iMac.local>
2024-12-03 16:34:34 +0800

    3

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const { userRouter } = require("../server/routes/userRouter");
+const { postRouter } = require("../server/routes/postRouter");
+const { commentRouter } = require("../server/routes/commentRouter");
+const { likeRouter } = require("../server/routes/likeRouter");
+
 dotenv.config();
 const connectDataBase = async () => {
   const res = await mongoose.connect(process.env.MONGODB_URI);
   if (res) console.log("db connected");
 };
 connectDataBase();
-const { userRouter } = require("../server/routes/userRouter");
-const { postRouter } = require("../server/routes/postRouter");
-const { commentRouter } = require("../server/routes/commentRouter");
-const { likeRouter } = require("../server/routes/likeRouter");
+
 const app = express();
 app.use(express.json());
 app.use("/user", userRouter);

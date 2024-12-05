54bf2b7df2cacce75dc03d7470571010a4f07aa0
admin <admin@admins-iMac.local>
2024-12-05 17:38:57 +0800

    5

--- controllers/post/posts.js
+++ controllers/post/posts.js
@@ -1,7 +1,27 @@
+const { postModel } = require("../../models/postSchema");
 const posts = async (req, res) => {
   const post = await postModel
     .find()
-    .populate("comments", "comment userId postId");
+    .populate({
+      path: "comments",
+      select: "comment userId postId ",
+      populate: {
+        path: "userId",
+        select: "username profileImage",
+      },
+    })
+    .populate({
+      path: "likes",
+      populate: {
+        path: "userId",
+        select: "username profileImage",
+      },
+    })
+    .populate({
+      path: "userId",
+      select: "username profileImage",
+    });
+
   res.send(post);
 };
 module.exports = { posts };

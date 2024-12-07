80a96ef994c58a9a6f771e6088f279c40d47f6e3
admin <admin@admins-iMac.local>
2024-12-07 12:53:58 +0800

    7

--- controllers/comment/getCommentOfOnePost.js
+++ controllers/comment/getCommentOfOnePost.js
@@ -1,7 +1,7 @@
 const { commentModel } = require("../../models/commentSchema");
 
 const getCommentOfOnePost = async (req, res) => {
-  const { id } = req.query;
+  const { id } = req.params;
   const comments = await commentModel.find({ postId: id });
   res.send(comments);
 };

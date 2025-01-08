ba2adcea24d17a7da134dea2ca9b7dacdd1071c8
khicheengui <latex0710@gmail.com>
2025-01-08 17:44:46 +0800

    1/8

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const { create } = require("../controllers/post/create");
 const { posts } = require("../controllers/post/posts");
-const {getPostsOfOneUser} = require("../controllers/post/getPostsOfOneUser");
+const { getPostsOfOneUser } = require("../controllers/post/getPostsOfOneUser");
 const { authToken } = require("../controllers/middleware/authorization");
 const postRouter = express();
 postRouter.post("/create", authToken, create);
 postRouter.get("/posts", authToken, posts);
-postRouter.get("/getPostsOfOneUser/:userId", getPostsOfOneUser);
+postRouter.get("/getPostsOfOneUser/:userId", authToken, getPostsOfOneUser);
 
 module.exports = { postRouter };
